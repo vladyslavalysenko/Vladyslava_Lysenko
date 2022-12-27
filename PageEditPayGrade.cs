f4ea5c981e3b135726d0a908860f72400df77665
vladyslavalysenko <76841973+vladyslavalysenko@users.noreply.github.com>
2022-12-27 21:48:53 +0200

    PageObjects

---
+++ PageEditPayGrade.cs
@@ -0,0 +1,77 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_selenium.PageObjects
+{
+    public class PageEditPayGrade
+    {
+        protected WebDriver driver;
+
+        private readonly By minSalaryBy = By.XPath("//div//child::div[text()='10000.00']");
+        private readonly By maxSalaryBy = By.XPath("//div//child::div[text()='12000.00']");
+        private readonly By deleteBy = By.XPath("//div//child::div[text()='10000.00']//ancestor::div[@role=\"row\"]//child::i[@class='oxd-icon bi-trash']");
+        private readonly By confirmDelete = By.XPath("//button[text()=' Yes, Delete ']");
+        private readonly By recordFoundBy = By.XPath("//span[text()='(1) Record Found']");
+        private readonly By cancelBy = By.XPath("//button[text()=' Cancel ']");
+        private readonly By nofoundBy = By.XPath("//span[text()='No Records Found']");
+
+        public PageEditPayGrade(WebDriver driver)
+        {
+            this.driver = driver;
+        }
+        public void Wait()
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.ElementIsVisible(recordFoundBy));
+        }
+        public void Wait1()
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.ElementIsVisible(nofoundBy));
+        }
+
+        public bool IsElementPresent(By el)
+        {
+            try
+            {
+                driver.FindElement(el);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+        public bool IsCurrencyPresent()
+        {
+            if (IsElementPresent(minSalaryBy) && IsElementPresent(maxSalaryBy))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+        public void DeleteCurrency()
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            driver.FindElement(deleteBy).Click();
+            wait.Until(ExpectedConditions.ElementIsVisible(confirmDelete));
+            driver.FindElement(confirmDelete).Click();
+        }
+        public PageMain GoToMainPage()
+        {
+            driver.FindElement(cancelBy).Click();
+            return new PageMain(driver);
+        }
+    }
+}
---
+++ PageMain.cs
@@ -0,0 +1,91 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
+namespace HW_selenium.PageObjects
+{
+    public class PageMain
+    {
+        protected WebDriver driver;
+
+        private readonly By adminBy = By.XPath("//span[text()='Admin']");
+        private readonly By jobBy = By.XPath("//span[text()='Job ']");
+        private readonly By jobPayGradesBy = By.XPath("//a[text()='Pay Grades']");
+        private readonly By jobPayGradesTitleBy = By.XPath("//h6[text()='Pay Grades']");
+        private readonly By AddBy = By.XPath("//button[text()=' Add ']");
+        private readonly By payGradesBy = By.XPath("//h6[text()='Pay Grades']");
+        private readonly By gradeCurrencyBy = By.XPath("//div[not(text()) and @data-v-c186142a]");
+        private readonly By deleteGradeBy = By.XPath("//div//child::div[text()='RandomName']//ancestor::div[@role='row']//child::i[@class='oxd-icon bi-trash']");
+        private readonly By confirmDeleteBy = By.XPath("//button[text()=' Yes, Delete ']");
+        private readonly By nameBy = By.XPath("//div[text()='Name']");
+        private readonly By gradeTitleBy;
+
+        public PageMain(WebDriver driver, string title = "RandomName")
+        {
+            this.driver = driver;
+            gradeTitleBy = By.XPath("//div//child::div[text()='" + title + "']");
+    }
+        public void GoToAdmin()
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.ElementIsVisible(adminBy));
+            driver.FindElement(adminBy).Click();
+        }
+        public void Wait()
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.ElementIsVisible(nameBy));
+        }
+        public void GoToPayGrades()
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.ElementIsVisible(jobBy));
+
+            driver.FindElement(jobBy).Click();
+            wait.Until(ExpectedConditions.ElementIsVisible(jobPayGradesBy));
+            driver.FindElement(jobPayGradesBy).Click();
+        }
+        public void GoToAddGrades()
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.ElementIsVisible(jobPayGradesTitleBy));
+            driver.FindElement(AddBy).Click();
+        }
+        public bool IsElementPresent(By el)
+        {
+            try
+            {
+                driver.FindElement(el);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+        public bool IsGradePresent()
+        {
+            if (IsElementPresent(gradeTitleBy) && IsElementPresent(gradeCurrencyBy))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        public void DeleteGrade()
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            driver.FindElement(deleteGradeBy).Click();
+            wait.Until(ExpectedConditions.ElementIsVisible(confirmDeleteBy));
+            driver.FindElement(confirmDeleteBy).Click();
+
+        }
+        public PageAddPayGrade GoToAddPayGradePage()
+        {
+            return new PageAddPayGrade(driver);
+        }
+    }
+}

65aaa8034d5196ae546bd902cec72584af3b1363
vladyslavalysenko <76841973+vladyslavalysenko@users.noreply.github.com>
2023-01-03 19:09:38 +0200

    Add files via upload

---
+++ WebAPI.hometask.structure/Responses/MetaData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_WebAPI.Structure.Responses
+{
+    public class MetaData
+    {
+        public string tag { get; set; }
+        public string name { get; set; }
+        public string path_lower { get; set; }
+        public string path_display { get; set; }
+        public string id { get; set; }
+        public DateTime client_modified { get; set; }
+        public DateTime server_modified { get; set; }
+        public string rev { get; set; }
+        public int size { get; set; }
+        public bool is_downloadable { get; set; }
+        public string content_hash { get; set; }
+    }
+}
---
+++ WebAPI.hometask.structure/RestApiClient/Authentication/AppAuthenticator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using RestSharp;
+using System.Threading.Tasks;
+using RestSharp.Authenticators;
+
+namespace HW_WebAPI.Application.RestApiClient.Common
+{
+    public class AppAuthenticator : AuthenticatorBase
+    {
+        readonly string _baseUrl;
+        readonly string _clientId;
+        readonly string _clientSecret;
+
+        public AppAuthenticator(string baseUrl, string clientId, string clientSecret) : base("")
+        {
+            _baseUrl = baseUrl;
+            _clientId = clientId;
+            _clientSecret = clientSecret;
+        }
+
+        protected override async ValueTask<Parameter> GetAuthenticationParameter(string accessToken)
+        {
+            Token = string.IsNullOrEmpty(Token) ? await GetToken() : Token;
+            return new HeaderParameter(KnownHeaders.Authorization, Token);
+        }
+
+        private async Task<string> GetToken()   
+        {
+            var options = new RestClientOptions(_baseUrl);
+            using var client = new RestClient(options)
+            {
+                Authenticator = new HttpBasicAuthenticator(_clientId, _clientSecret),
+            };
+
+            var request = new RestRequest("2/auth/token/from_oauth1")
+                .AddParameter("grant_type", "client_credentials");
+            var response = await client.PostAsync<TokenResponse>(request);
+            return $"{response!.TokenType} {response!.AccessToken}";
+        }
+    }
+}
+

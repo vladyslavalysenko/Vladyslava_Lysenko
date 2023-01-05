031247bae8b4818b3621e2ba4dda3352f26da205
vladyslavalysenko <76841973+vladyslavalysenko@users.noreply.github.com>
2023-01-05 09:42:23 +0200

    Add files via upload

---
+++ BuildReq.cs
@@ -0,0 +1,41 @@
+﻿using RestSharp;
+
+namespace HW_API
+{
+    internal class BuildReq
+    {
+        private RestRequest request;
+
+        public BuildReq SetUrl(string url)
+        {
+            request = new RestRequest(url, Method.Post);
+            return this;
+        }
+
+        public BuildReq SetHeader(string key, string value)
+        {
+            request.AddHeader(key, value);
+            return this;
+        }
+
+
+        public BuildReq SetBody<T>(T body) where T : class
+        {
+            request.AddJsonBody(body);
+            return this;
+        }
+
+        public BuildReq SetFile(string filePath)
+        {
+            byte[] fileData = File.ReadAllBytes(filePath);
+            request.AddParameter("application/octet-stream", fileData, ParameterType.RequestBody);
+            return this;
+        }
+
+        public RestRequest Build()
+        {
+            return request;
+        }
+
+    }
+}
---
+++ ResMetaData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace HW_API
+{
+    public class DeleteFileMetaData
+    {
+        public MetaData metadata { get; set; }
+    }
+
+    public class MetaData
+    {
+        [JsonPropertyName(".tag")]
+        public string tag { get; set; }
+        public DateTime client_modified { get; set; }
+        public string content_hash { get; set; }
+        public bool has_explicit_shared_members { get; set; }
+        public string id { get; set; }
+        public bool is_downloadable { get; set; }
+        public string name { get; set; }
+        public string path_display { get; set; }
+        public string path_lower { get; set; }
+        public string rev { get; set; }
+        public DateTime server_modified { get; set; }
+        public int size { get; set; }
+    }
+}
